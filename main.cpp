e491a11db6c2a42ed9df79ca999703857fca459b
courageousstrawberry <elech2a@gmail.com>
2024-10-27 21:22:59 -0700

    add timing using chrono

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 #include <vector>
 #include <fstream>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
     set<string> codes_set;
     vector<string> codes_vector;
     list<string> codes_list;
+
+    auto start_list = high_resolution_clock::now();
+    auto start_vector = high_resolution_clock::now();
 
     // Reading race
     ifstream infile("codes.txt");
@@ -20,6 +24,8 @@ int main() {
         }
         infile.close()
     }
+    auto end_list = high_resolution_clock::now();
+    auto duration_list = duration_cast<milliseconds>(end_list - start_list).count();
 
     return 0;
 }
