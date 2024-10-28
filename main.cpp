9265f60b65c7e9de2f66a939ee15221392b8e8e4
courageousstrawberry <elech2a@gmail.com>
2024-10-27 21:15:36 -0700

    begin reading race

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <chrono>
+#include <set>
+#include <list>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 int main() {
-    
+    set<string> codes_set;
+    vector<string> codes_vector;
+    list<string> codes_list;
+
+    // Reading race
+    ifstream infile("codes.txt");
+    if (infile.is_open()) {
+        string code;
+        while (getline(infile, code)) {
+            codes_vector.push_back(code);
+        }
+        infile.close()
+    }
 
     return 0;
 }
